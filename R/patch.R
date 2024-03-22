4f442c9d6a645abed6490ba9d1db519e40058fd4
Jack Wardle <j.wardle19@imperial.ac.uk>
2024-03-22 12:50:43 +0000

    edits to some documentation

--- R/patch.R
+++ R/patch.R
@@ -95,23 +95,38 @@ patch <- function(s_patch,
 ##'
 ##' @param s_patch Number of susceptible individuals in patch
 ##' @param e_patch Number of exposed individuals in patch
-##' @param i_patch Number of infected individuals in patch
+##' @param i_a_patch Number of asymptomatic infected individuals in patch
+##' @param i_p_patch Number of presymptomatic infected individuals in patch
+##' @param i_s_patch Number of symptomatic infected individuals in patch
 ##' @param r_patch Number of recovered individuals in patch
 ##' @param birth_rate Birth rate for this patch. Number of births per unit time
 ##' @param death_rate Death rate for this patch. Number of deaths per unit time in this patch
 ##' @param transmission_rate effective contact rate
+##' @param asymptomatic_infectiousness Proportion giving 
+##' the infectivity of asymptomatic cases relative to
+##' pre-symptomatic and symptomatic cases.
+##' @param prop_symptomatic Proportion of cases that are symptomatic.
 ##' @param infection_rate rate of moving from compartment E to I
-##' @param recovery_rate rate of moving from compartment I to R.
+##' @param symptom_rates Rate of moving from I_presymptomatic to I_symptomatic.
+##' @param recovery_rates_asym Rate of moving from I_asymtpomatic to recovered
+##' @param recovery_rates_sym Rate of moving from I_symtpomatic to recovered
 ##' @return a list of class patch which has the following items
 ##' ** susceptible ** Number of susceptibles in this patch
 ##' ** exposed ** Number of exposed individuals in this patch
-##' ** infected ** Number of infected individuals in this patch
+##' ** infected_asymptomatic ** Number of asymptomatic infected individuals in this patch
+##' ** infected_presymptomatic ** Number of presymptomatic infected individuals in this patch
+##' ** infected_symptomatic ** Number of symptomatic infected individuals in this patch
 ##' ** recovered ** Number of recovered individuals in this patch
 ##' ** birth_rate ** Patch-specific birth rate
 ##' ** death_rate ** Patch-specific death rate
 ##' ** transmission_rate ** Effective contact rate for this patch
-##' ** Infection rate ** Rate at which individuals move from E to I
-##' ** Recovery rate ** Rate at which individuals move from  I to R.
+##' ** asymptomatic_infectiousness ** Relative infectivity of asymptomatic infected individuals
+##' ** prop_symptomatic ** Proportion of infections with symptoms
+##' ** Infection rate ** Rate at which individuals move from E to I_a or I_p
+##' ** Symptom rate ** Rate at which individuals move from I_p to I_s
+##' ** Asymtpomatic recovery rate ** Rate at which individuals move from  I_a to R.
+##' ** Symtpomatic recovery rate ** Rate at which individuals move from  I_s to R.
+##' 
 ##' @export
 ##' @author Jack Wardle
 patch_symptoms <- function(s_patch,
